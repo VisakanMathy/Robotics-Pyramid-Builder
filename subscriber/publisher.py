c94544cc8ef84e8753a138e47fbdefe0da816192
VisakanMathy <32844998+VisakanMathy@users.noreply.github.com>
2019-03-11 17:09:29 +0000

    Update publisher.py

--- subscriber/publisher.py
+++ subscriber/publisher.py
@@ -6,7 +6,6 @@ from std_msgs.msg import Int32
 pub = rospy.Publisher('chatter', Int32, queue_size = 10)
 rospy.init_node('Starter_node')
 r = rospy.Rate(10)
-layer= input(int('how many layers?'))
 while not rospy.is_shutdown():
-    pub.publish(layer)
+    pub.publish(4)
     r.sleep()
