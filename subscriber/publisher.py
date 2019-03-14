de9e51f4263c716f75ee6b8ea5557151e9e8251c
GJones97 <47558544+GJones97@users.noreply.github.com>
2019-03-14 10:08:53 +0000

    Update publisher.py

--- subscriber/publisher.py
+++ subscriber/publisher.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Int32
 
+#Initializing the robot into the start position
 pub = rospy.Publisher('chatter', Int32, queue_size = 10)
 rospy.init_node('Starter_node')
 r = rospy.Rate(10)
