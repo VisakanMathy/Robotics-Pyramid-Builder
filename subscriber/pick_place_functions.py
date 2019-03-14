1f9f5099ba2d46d5d0b78423e3dd9c8ccbeb46d8
VisakanMathy <32844998+VisakanMathy@users.noreply.github.com>
2019-03-14 15:41:16 +0000

    Update pick_place_functions.py

--- subscriber/pick_place_functions.py
+++ subscriber/pick_place_functions.py
@@ -40,6 +40,7 @@ import copy
 import numpy as np
 import math
 import time
+
 import rospy
 import rospkg
 
@@ -94,12 +95,9 @@ class PickAndPlace(object):
             start_angles = dict(zip(self._joint_names, [0]*7))
         self._guarded_move_to_joint_position(start_angles)
         #self.gripper_open()
-        rospy.sleep(1.0)
+        #rospy.sleep(1.0)
         print("Running. Ctrl-c to quit")
 
-        
-    ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
-    
     def ik_request(self, pose):
         hdr = Header(stamp=rospy.Time.now(), frame_id='base')
         ikreq = SolvePositionIKRequest()
@@ -132,8 +130,7 @@ class PickAndPlace(object):
             rospy.logerr("INVALID POSE - No Valid Joint Solution Found.")
             return False
         return limb_joints
-    
- ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
+
     def _guarded_move_to_joint_position(self, joint_angles):
         if joint_angles:
             self._limb.move_to_joint_positions(joint_angles)
@@ -148,8 +145,6 @@ class PickAndPlace(object):
         self._gripper.close()
         rospy.sleep(1.0)
 
-  ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
-            
     def _approach(self, pose):
         approach = copy.deepcopy(pose)
         # approach with a pose the hover-distance above the requested pose
@@ -171,25 +166,20 @@ class PickAndPlace(object):
         joint_angles = self.ik_request(ik_pose)
         # servo up from current pose
         self._guarded_move_to_joint_position(joint_angles)
-        
- ## The next function definition is a quick check to test whether the end effector has actually gone to the coordinates specified. 
-## More information about the following lines of code can be found in the documentation under 'Our process'.
+
     def print_position(self):
         current_pose = self._limb.endpoint_pose()
-        print(current_pose) #this is the current pose of the end effector
-        x = current_pose['position'].x 
+        pprint(current_pose)
+        x = current_pose['position'].x
         y = current_pose['position'].y
         z = current_pose['position'].z
-        print(x, y, z) #this prints out the x, y and z values of the end effector location.
-        
-        
- ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
+        print(x, y, z)
+
     def _servo_to_pose(self, pose):
         # servo down to release
         joint_angles = self.ik_request(pose)
         self._guarded_move_to_joint_position(joint_angles)
-        
- ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
+
     def pick(self, pose):
         self._hover_distance = self._hover_distance/2
         # open the gripper
@@ -204,7 +194,6 @@ class PickAndPlace(object):
         self._retract()
         self._hover_distance = self._hover_distance*2
 
-  ## More information about the following lines of code can be found in the documentation under 'Pre-existing Demo Code'.
     def place(self, pose):
         # servo above pose
         self._approach(pose)
@@ -215,64 +204,81 @@ class PickAndPlace(object):
         # retract to clear object
         self._retract()
 
- ## This loads the cafe table. More information about the following lines of code can be found in the documentation under 'Setting up the environment'.
-def load_gazebo_models(table_pose=Pose(position=Point(x=0.75, y=0, z=0)),
+        
+def load_gazebo_models(table_pose=Pose(position=Point(x=0.68, y=0, z=0)),
                        table_reference_frame="world"):
     # Get Models' Path
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
-    # Load Table URDF
+    # Load Table SDF
     table_xml = ''
     with open (model_path + "cafe_table/table.urdf", "r") as table_file:
         table_xml=table_file.read().replace('\n', '')
+
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try: #Spawn the table into gazebo
+    try:
         spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
         resp_sdf = spawn_sdf("cafe_table", table_xml, "/",
-                             table_pose, table_reference_frame) 
+                             table_pose, table_reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn SDF service call failed: {0}".format(e))
 
         
-  ## This loads the brick. More information about the following lines of code can be found in the documentation under 'Setting up the environment'.       
 def load_brick_at_starting_point(brick_number, brick_pose=Pose(position=Point(x=0.49, y=0, z=1)),
                 brick_reference_frame =  'world'):
 
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
-    # Load brick URDF into the starting position
+    # Load Table SDF
     
     brick_xml = ''
+
     with open (model_path + "new_brick/model.sdf", "r") as brick_file:
         brick_xml=brick_file.read().replace('\n', '')
-    #Spawn the brick into the starting position
+
     try:
         spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
         resp_sdf = spawn_sdf("new_brick_left{}".format(brick_number), brick_xml, "/",
-                             brick_pose, brick_reference_frame)  
+                             brick_pose, brick_reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn SDF service call failed: {0}".format(e))
         # Spawn Block URDF
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
 
- ## Orientates the brick. More information about the following lines of code can be found in the documentation under 'Orientation of end-effector'.
+
 def load_orientated_brick(brick_number, brick_pose=Pose(position=Point(x=0.49, y=0.0, z=1)),brick_reference_frame =  'world'):
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
-    # Load brick URDF into the correct position
+    # Load Table SDF
     
     brick_xml = ''
 
     with open (model_path + "new_brick/rotate_brick.urdf", "r") as brick_file:
         brick_xml=brick_file.read().replace('\n', '')
-    # Spawn Brick URDF into the correct position
+
     try:
         spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
         resp_sdf = spawn_sdf("new_brick{}".format(brick_number), brick_xml, "/",
                              brick_pose, brick_reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn URDF service call failed: {0}".format(e))
-        
+        # Spawn Block URDF
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
 
- ## More information about the following lines of code can be found in the documentation under 'Deleting the model and bricks'
+def load_brick_in_structure(brick_number,x,y,z):
+    model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
+    brick_pose=Pose(position=Point(x, y, z))
+    brick_reference_frame =  'world'
+    brick_xml = ''
+
+    with open (model_path + "new_brick/brick.urdf", "r") as brick_file:
+        brick_xml=brick_file.read().replace('\n', '')
+
+    try:
+        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
+        resp_sdf = spawn_sdf("struct_brick{}".format(brick_number), brick_xml, "/",
+                             brick_pose, brick_reference_frame)
+    except rospy.ServiceException, e:
+        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
+
+
 def delete_gazebo_models():
     # This will be called on ROS Exit, deleting Gazebo models
     # Do not wait for the Gazebo Delete Model service, since
@@ -284,21 +290,27 @@ def delete_gazebo_models():
     except rospy.ServiceException, e:
         rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
- ## More information about the following lines of code can be found in the documentation under 'Deleting the model and bricks'
+
 def delete_bricks(brick):
     for item in range(brick):
         try:
             resp_delete = delete_model('new_brick{}'.format(item))
         except rospy.ServiceException, e:
             rospy.loginfo("Delete Model service call failed: {0}".format(e))
+
+def delete_brick(brick):
+    print(brick)
+    try:
+        delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+        resp_delete = delete_model('new_brick{}'.format(brick))
+    except rospy.ServiceException, e:
+        rospy.loginfo("Delete Model service call failed: {0}".format(e))
      
 def listen_to_hub(topic_number):
     print("listening to hub for {}".format(topic_number))
     msg = rospy.wait_for_message('do_shit{}'.format(topic_number),Int64MultiArray)
     value = msg.data[0]
     return value
-
-# SEE COORDINATE GENERATION OF DOCUMENTATION - explains logic of these two functions in detail.
 
 def create_coordinates_new(layer):
     sc = 1; #this scales all the brick dimensions and the gaps between the bricks
@@ -312,8 +324,8 @@ def create_coordinates_new(layer):
     zb = zbrick*sc
     #starting position`
     xs = 0.49; #this is the starting position along length
-    ys = 0.01; #starting position along height
-    zs = 0.11; #starting position along width
+    ys = 0.0; #starting position along height
+    zs = 0.09; #starting position along width
 
     s = [xs, ys, zs] #this is the permanent starting position which is reeferenced as the centre of the brick
 
@@ -325,18 +337,18 @@ def create_coordinates_new(layer):
     l = layers.tolist() #this is a layersist of number of bricks in each layer. e.g. [3, 2, 1] is 3 bricks in base layer, 2 in second etc
     m = [d*2 for d in l]
     lay = [] #this is the matrix containint all the coordinates for the pyramid in x, y, z which corrrespond to length, depth, width with the brick normally orientated
-    x_structure = round(xs + xb + sc*0.05,4)
+    x_structure = round(xs + xb/2 + sc*0.01,4)
 
     for i in m:
 
-        z_structure = zs + (layer-(i/2))*zb + 0.03 # this finds the height of layers of brick
-        pos_list = list(range(int((i)/2))) # creates a range of positive integers for left arm
-        neg_list = list(range(int(-((i)/2)),0)) # reates a range of negative integers for right arm
+        z_structure = zs + (layer-(i/2))*zb + 0.03
+        pos_list = list(range(int((i)/2)))
+        neg_list = list(range(int(-((i)/2)),0))
         tot_list = neg_list + pos_list
-        total_list = [x+0.5 for x in tot_list] # shifts all these integers by 0.5 so the bricks are put in 'even' positions
+        total_list = [x+0.5 for x in tot_list]
 
         for item in total_list:
-            y_structure = ys + item*(yb + 0.03) # finds y width of brick 
+            y_structure = ys + item*(yb + 0.03)
 
             c = [round(x_structure, 4), round(y_structure, 4), round(z_structure, 4)]
 
@@ -344,6 +356,7 @@ def create_coordinates_new(layer):
             cnew = c
             lay.append(cnew)
     return lay, s
+
 
 def create_coordinates(layer):
     sc = 1; #this scales all the brick dimensions and the gaps between the bricks
@@ -371,80 +384,80 @@ def create_coordinates(layer):
     m = layers.tolist() #this is a list of number of bricks in each layer. e.g. [3, 2, 1] is 3 bricks in base layer, 2 in second etc
 
     lay = [] #this is the matrix containint all the coordinates for the pyramid in x, y, z which corrrespond to length, depth, width with the brick normally orientated
-    x_structure = round(xs + xb + sc*0.05,4) #the x coordinate for all bricks is the same 
+    x_structure = round(xs + xb + sc*0.05,4)
 
-    for j in range(x): #this is a for loop of length(number of layers)
-        z_structure = zs + (x-j-1)*(zb) + 0.03 #this is the z height of the brick. This depends on the layer number. We also included a 0.03 gap between the new brick and the layer below so as not to knock it
+    for j in range(x):
+        z_structure = zs + (x-j-1)*(zb) + 0.03
         
         if (j+1)%2 == 0: #even layers
-            init_list = list(range(int((j+1)/2))) #produces a list of the right size
+            init_list = list(range(int((j+1)/2)))
             rev_list = list(range(int((j+1)/2)))
 
             for i in range(len(init_list)):
-                init_list[i] = i+0.5 #list which is half size of layer. This produces 0.5, 1.5, 2.5 etc i.e. a shift of 0.5 brick lengths on each position compared to the odd layer below
-                rev_list[i] = -(i+0.5) #list which is half size of layer. This produces -0.5, -1.5, -2.5 etc i.e. a shift of 0.5 brick lengths on each position compared to the odd layer below
+                init_list[i] = i+0.5
+                rev_list[i] = -(i+0.5) 
             
-            rev_list.reverse()   #reversed so it goes from smallest neg value to largest, e.g. -2.5, -1.5, -0.5 making this a mirror of init_list
-            total_list = rev_list+init_list #add lists
+            rev_list.reverse()   
+            total_list = rev_list+init_list
 
         elif (j+1)%2 == 1: #odd layers
-            pos_list = list(range(int((j+2)/2))) #creates list of 1, 2, 3, 4, etc (j cannot be an odd number)
-            neg_list = list(range(int(-((j)/2)),0)) #creates list of 0, -1, -2, -3, -4, etc (j cannot be an odd number)
+            pos_list = list(range(int((j+2)/2)))
+            neg_list = list(range(int(-((j)/2)),0))
             total_list = neg_list + pos_list
             
         for item in total_list:
-            y_structure = ys + item*(yb + 0.03)   #then we times the item in total list (i.e. our init_list, rev)list, pos_list or neg_list value) with the brick width and gap
-            c = [round(x_structure,4), round(y_structure,4), round(z_structure,4)] #c is the coordinate
+            y_structure = ys + item*(yb + 0.03)   
+            c = [round(x_structure,4), round(y_structure,4), round(z_structure,4)]
             
             cnew = [] #Creates a new matrix
             cnew = c #Makes cnew equal to the c coordinates
-            lay.append(cnew) #appends cnew to lay
+            lay.append(cnew)
             
       
     lay.reverse()  
     print('lay', lay)
     return lay, s
 
-def arrange_coordinates(lay): #arranges coordinates so they are ready to be processed by the arms
-    RArm = [] # coordinates for right arm
-    LArm = [] # coordinates for left arm
+def arrange_coordinates(lay):
+    RArm = []
+    LArm = []
 
-    while len(lay) != 0: #whilst there are values in lay
-        z_smallest = lay[0][2] 
-        list_of_index = [] 
-        for i in range(len(lay)): 
-            if lay[i][2] < z_smallest: #ordering the values into list_of_index based on smallest z height value
+    while len(lay) != 0:
+        z_smallest = lay[0][2]
+        list_of_index = []
+        for i in range(len(lay)):
+            if lay[i][2] < z_smallest:
                 list_of_index = [i]
             elif lay[i][2] == z_smallest:
                 list_of_index.append(i)
                    
-        y_largest_index = list_of_index[0] 
+        y_largest_index = list_of_index[0]
         
-        for item in list_of_index: 
-            if lay[item][1] > lay[y_largest_index][1]: # we now order based on the largest y value. Large positive y values are appended to the left arm. 
+        for item in list_of_index:
+            if lay[item][1] > lay[y_largest_index][1]:
                 y_largest_index = item
-        LArm.append(lay[y_largest_index])  #This value is put into LArm
-        lay.remove(lay[y_largest_index])    #this coordinate is removed from lay ready for the next loop
+        LArm.append(lay[y_largest_index])
+        lay.remove(lay[y_largest_index])    
               
-        if len(lay) != 0: #if there is still values in lay (i.e. if the for loop above removes the last cordinate, the code should skip this if statement (produces list out of range error otherwise)
+        if len(lay) != 0:
             z_smallest = lay[0][2]
             list_of_index = []
             for i in range(len(lay)):
-                if lay[i][2] < z_smallest: #ordering the values into list_of_index based on smallest z height value
+                if lay[i][2] < z_smallest:
                     list_of_index = [i]
                 elif lay[i][2] == z_smallest:
                     list_of_index.append(i)
         
             y_smallest_index = list_of_index[0]
             for item in list_of_index:
-                if lay[item][1] < lay[y_smallest_index][1]: # we now order based on the largest y value. Those most negative will be given to the right arm. This means the arms will work 'inwards' which will look cool.
+                if lay[item][1] < lay[y_smallest_index][1]:
                     y_smallest_index = item
-            RArm.append(lay[y_smallest_index]) #This value is put into RArm
-            lay.remove(lay[y_smallest_index])  #this coordinate is removed from lay ready for the next loop
+            RArm.append(lay[y_smallest_index])
+            lay.remove(lay[y_smallest_index])  
 
     print('Rarm',RArm)
     print('LArm',LArm)
-    return RArm, LArm    #these are then used by the left and right arm
+    return RArm, LArm    
 
                 
                     
